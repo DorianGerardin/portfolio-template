5ca7bca3c12bb96389ed24d01d9682ecf8ac3470
Dorian Gerardin <gryfadon@gmail.com>
2025-09-27 17:34:58 +0200

    Add of templates and portfolio config file

---
+++ static/js/config.js
@@ -0,0 +1,101 @@
+portfolioTemplate = {
+    info: {
+        fullName: "Maxime Gerardin",
+        personalDescription: "3D and Environment freelance artist based in south of France, creating immersive visuals for personal projects and brands.",
+        logo: "/static/assets/logo.jpg",
+        logoCircle: true,
+        logoAlt: "Logo icon"
+    },
+    socials: [
+        {
+            text: "ArtStation",
+            icon: "/static/assets/icons/artstation.svg",
+            url: "https://www.artstation.com/maximegerardin"
+        },
+        {
+            text: "Instagram",
+            icon: "/static/assets/icons/instagram.svg",
+            url: "https://www.instagram.com/maxime.gerardin"
+        },
+        {
+            text: "LinkedIn",
+            icon: "/static/assets/icons/linkedin.svg",
+            url: "https://www.linkedin.com/in/maximusgerardin/"
+        },
+        {
+            text: "Patreon",
+            icon: "/static/assets/icons/patreon.svg",
+            url: "https://www.patreon.com/maximegerardin"
+        },
+        {
+            text: "Youtube",
+            icon: "/static/assets/icons/youtube.svg",
+            url: "https://www.youtube.com/@maximusgerardin"
+        }
+    ],
+    projects: [
+        {
+            name: "Valorant",
+            videoThumbnail: "/static/assets/thumbnails/valorant.mp4",
+            videoThumbnailExt: "mp4",
+            types: ["3D", "Animation"],
+            year: "2024",
+            description: "blabla",
+            client: "Riot Games",
+            additionalMedias: [],
+            additionalLinks: [],
+        },
+        {
+            name: "Island",
+            videoThumbnail: "/static/assets/thumbnails/island.mp4",
+            videoThumbnailExt: "mp4",
+            types: ["3D", "Animation"],
+            year: "2024",
+            description: "blabla",
+            client: "",
+            additionalMedias: [],
+            additionalLinks: [],
+        },
+        {
+            name: "Secret Lab",
+            videoThumbnail: "/static/assets/thumbnails/secret-lab.mp4",
+            videoThumbnailExt: "mp4",
+            types: ["3D", "Animation"],
+            year: "2024",
+            description: "blabla",
+            client: "Riot Games",
+            additionalMedias: [],
+            additionalLinks: [],
+        },
+        {
+            name: "Horse",
+            videoThumbnail: "/static/assets/thumbnails/horse.mp4",
+            videoThumbnailExt: "mp4",
+            types: ["3D", "Animation"],
+            year: "2024",
+            description: "blabla",
+            client: "Riot Games",
+            additionalMedias: [],
+            additionalLinks: [],
+        }
+    ],
+    footer: {
+        contact: [
+            {
+                icon: "/static/assets/icons/location.svg",
+                url: "",
+                text: "South of France"
+            },
+            {
+                icon: "/static/assets/icons/mail.svg",
+                url: "mailto:maximegerardincontact@gmail.com",
+                text: "maximegerardincontact@gmail.com"
+            }
+        ]
+    }
+}
+
+styleConfig = {
+    portfolioFont: { name: 'Outfit', weights: '100..900', useWeight: '400' }, // Google fonts
+    fullNameFont: { name: 'Outfit', weights: '100..900', useWeight: '900'} // Google fonts
+}
--- static/js/main.js
+++ static/js/main.js
@@ -11,14 +11,14 @@ let observer = new IntersectionObserver((entries) => {
             visibles.delete(entry.target);
         }
     });
-}, { threshold: 0.3 });
+}, { threshold: 1 });
 
 
 function highlightMostCentered() {
     let center = window.innerHeight / 2;
     let closest = null;
     let closestDistance = Infinity;
-    let workBrightness = style.getPropertyValue('--work-brightness')
+    let projectBrightness = style.getPropertyValue('--project-brightness')
 
     visibles.forEach(card => {
         let rect = card.getBoundingClientRect();
@@ -31,16 +31,16 @@ function highlightMostCentered() {
         }
     });
 
-    document.querySelectorAll(".work").forEach(c => {
-        c.querySelectorAll(".work-text").forEach(v => {
+    document.querySelectorAll(".portfolio-project").forEach(c => {
+        c.querySelectorAll(".project-text").forEach(v => {
             v.classList.replace("opaque", "transparent");
         });
-        c.style.filter = `brightness(${workBrightness})`
+        c.style.filter = `brightness(${projectBrightness})`
     });
 
     if (closest) {
         closest.style.filter = "brightness(1)";
-        closest.querySelectorAll(".work-text").forEach(v => {
+        closest.querySelectorAll(".project-text").forEach(v => {
             v.classList.replace("transparent", "opaque");
         });
     }
@@ -55,20 +55,11 @@ window.addEventListener("scroll", () => {
     }
 });
 
-document.querySelectorAll(".work").forEach(div => {
-    observer.observe(div);
-});
-
-window.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".work").forEach(v => {
-        v.classList.add("visible");
-    });
-});
 
 
 
 
-const sections = document.querySelectorAll(".work");
+let sections = [];
 let index = 0;
 let isScrolling = false;
 
@@ -116,3 +107,172 @@ window.addEventListener("wheel", (e) => {
         unlockScroll()
     }, 300);
 });
+
+
+function loadFonts(...fonts) {
+    const display = 'swap';
+
+    const families = fonts.map(font => {
+        if (typeof font === 'object' && font.name) {
+            const name = font.name.replace(/ /g, '+');
+            return font.weights
+                ? `${name}:wght@${font.weights}`
+                : name;
+        }
+        return font.replace(/ /g, '+');
+    }).join('&family=');
+
+    const url = `https://fonts.googleapis.com/css2?family=${families}&display=${display}`;
+    const link = document.createElement('link');
+    link.rel = 'stylesheet'; link.href = url;
+    document.head.appendChild(link);
+}
+
+
+function slugify(text) {
+    return text.toLowerCase().trim().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, '');
+}
+
+function hasNonEmptyString(obj) {
+    return Object.values(obj).some(value => {
+        if (typeof value === "string") return value.trim() !== "";
+        if (typeof value === "object" && value !== null) return hasNonEmptyString(value);
+        return false;
+    });
+}
+
+function fillHeader()
+{
+    let portfolioLogo = document.getElementById("portfolio-logo");
+    if("info" in portfolioTemplate) {
+        portfolioLogo.src = portfolioTemplate.info.logo;
+        portfolioLogo.alt = portfolioTemplate.info.logoAlt;
+        if (portfolioTemplate.info.logoCircle) {
+            portfolioLogo.style.borderRadius = "50%";
+        }
+    }
+}
+
+function fillAbout()
+{
+    if("info" in portfolioTemplate) {
+        let portfolioName = document.getElementById("portfolio-name");
+        let portfolioDescription = document.getElementById("portfolio-description");
+
+        portfolioName.innerText = portfolioTemplate.info.fullName
+        portfolioDescription.innerText = portfolioTemplate.info.personalDescription
+    }
+    if("socials" in portfolioTemplate) {
+        let portfolioSocials = document.getElementById("portfolio-socials");
+        portfolioTemplate.socials.forEach(social => {
+            const socialTemplate = document.getElementById("portfolio-social-template");
+            let socialClone = socialTemplate.content.cloneNode(true);
+            let socialIcon = socialClone.querySelector(".portfolio-social-icon")
+            let socialLink = socialClone.querySelector(".portfolio-social-link")
+            socialLink.href = social.url
+            socialIcon.src = social.icon
+            portfolioSocials.appendChild(socialClone)
+        })
+    }
+}
+
+function fillProjects ()
+{
+    if("projects" in portfolioTemplate) {
+        let projectsNode = document.getElementById("projects");
+        portfolioTemplate.projects.forEach(project => {
+            const projectTemplate = document.getElementById("portfolio-project-template");
+            let projectClone = projectTemplate.content.cloneNode(true);
+            let projectElement = projectClone.querySelector(".portfolio-project")
+            let projectLink = projectClone.querySelector(".portfolio-project-link")
+            let projectThumbnail = projectClone.querySelector(".portfolio-project-thumbnail")
+            let projectTitle = projectClone.querySelector(".project-title")
+            let projectTag = projectClone.querySelector(".project-tag")
+
+            projectLink.href = `/project.html#${slugify(project.name)}`
+            projectThumbnail.src = project.videoThumbnail
+            projectThumbnail.type = `video${project.videoThumbnailExt}`
+            projectTitle.innerText = project.name
+            projectTag.innerText = project.types?.[0]
+            observer.observe(projectElement);
+            sections.push(projectElement);
+            projectsNode.appendChild(projectClone)
+            requestAnimationFrame(() => {
+                projectElement.style.opacity = "1";
+            });
+        })
+    }
+}
+
+function fillFooterSection(sectionID, templateObj){
+    let footerSectionContact = document.getElementById(sectionID)
+    if (hasNonEmptyString(templateObj))
+    {
+        templateObj.forEach(obj => {
+            if (hasNonEmptyString(obj)) {
+                let footerItemTemplate
+                let footerItemClone
+                if(obj.url !== "") {
+                    footerItemTemplate = document.getElementById("footer-section-item-link-template");
+                    footerItemClone = footerItemTemplate.content.cloneNode(true);
+                    let footerItemLink = footerItemClone.querySelector(".footer-section-item-link")
+                    footerItemLink.href = obj.url
+                }
+                else {
+                    footerItemTemplate = document.getElementById("footer-section-item-template");
+                    footerItemClone = footerItemTemplate.content.cloneNode(true);
+                }
+                let footerItemIcon = footerItemClone.querySelector(".footer-section-item-icon")
+                let footerItemText = footerItemClone.querySelector(".footer-section-item-text")
+                footerItemIcon.src = obj.icon
+                footerItemText.innerText = obj.text
+                footerSectionContact.querySelector(".footer-section-list").appendChild(footerItemClone)
+            }
+        })
+    }
+    else {
+        footerSectionContact.style.display = "none";
+    }
+}
+
+function fillFooter() {
+
+    if("footer" in portfolioTemplate) {
+        let footerName = document.getElementById("footer-portfolio-name");
+        let footerCopyName = document.getElementById("portfolio-copy-footer-name");
+        let footerCopyYear = document.getElementById("portfolio-copy-year-footer");
+        footerName.innerText = portfolioTemplate.info.fullName
+        footerCopyName.innerText = ` ${portfolioTemplate.info.fullName}`
+        footerCopyYear.textContent = new Date().getFullYear().toString()
+
+        fillFooterSection("footer-section-contact", portfolioTemplate.footer.contact)
+        fillFooterSection("footer-section-socials", portfolioTemplate.socials)
+    }
+}
+
+
+function fillPortfolioInfo() {
+    fillHeader()
+    fillAbout()
+    fillProjects()
+    fillFooter()
+}
+
+function applyConfigFonts()
+{
+    loadFonts(styleConfig.portfolioFont, styleConfig.fullNameFont);
+    document.querySelectorAll(".full-name").forEach(el => {
+        el.style.fontFamily = styleConfig.fullNameFont.name;
+        el.style.fontWeight = styleConfig.fullNameFont?.useWeight ?? 'normal';
+    })
+    document.body.style.fontFamily = styleConfig.portfolioFont.name;
+    document.body.style.fontWeight = styleConfig.portfolioFont?.useWeight ?? 'normal';
+}
+
+function applyConfigStyle()
+{
+    applyConfigFonts()
+}
+
+fillPortfolioInfo()
+applyConfigStyle()
